e741870d9bf51221a552aa455f8c6475caa14ac7
hmah689 <hmah689@aucklanduni.ac.nz>
2024-09-18 19:29:25 +1200

    integration is doing things but not really working

--- ProcessData.py
+++ ProcessData.py
@@ -1,40 +1,36 @@
 import matplotlib.pyplot as plt
 import scienceplots 
+from scipy import integrate
 import numpy as np
 import os
 
+
+
 folder = 'NotShorted'
-# subfolder = '100AC'
-# filename = '1AFreq.txt'
-
-# subfolderList = ['10AC','20AC','30AC','40AC','50AC','60AC','70AC','80AC','90AC','100AC']
-# filenameList = ['0AFreq.txt','1AFreq.txt','5AFreq.txt','10AFreq.txt','15AFreq.txt']
-
 subfolderList = ['10AC','50AC','90AC']
 filenameList = ['0ATime.txt','1ATime.txt','5ATime.txt','10ATime.txt']
 
-# subfolderList = ['10AC']
-# filenameList = ['0AFreq.txt','1AFreq.txt','5AFreq.txt','10AFreq.txt']
-
 
 for subfolder in subfolderList:
+
+    #make result directory
     resultPath = subfolder + " Freq Plots"
     try:
         os.mkdir(resultPath)
     except:
         print('Directory Exists')
-
-
+    #file
     plt.style.use('science')
-    # Adjust the spacing between subplots
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.4, hspace=2)
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.4, hspace=2)     # Adjust the spacing between subplots
     # `wspace` controls the width space between columns
     # `hspace` controls the height space between rows
     fig, ax = plt.subplots(4,1,figsize=(15,10))
-    n = 0
     fig.suptitle(subfolder + " Frequency Spectrum", fontsize=14, y=0.9)  # Adjust y to lower the title
 
+    n = 0
     for filename in filenameList:
+
+        #Get the data
         dataPath = os.path.join(folder,subfolder,filename)
         with open(dataPath,'r') as file:
             data = file.readlines()
@@ -48,7 +44,7 @@ for subfolder in subfolderList:
             cols = line.split('\t')
             dataArray = np.vstack([dataArray,cols]) #vertically append
 
-        # index = np.where(dataArray[2:,1] == 1)[0] #find index for 1 second
+
 
         time = np.array(dataArray[2:,0],dtype= float)
         index = np.where(time == 0.5)[0] #find index for 1 second
@@ -56,7 +52,10 @@ for subfolder in subfolderList:
         x = np.array(time[0:index[0]],dtype= float)
         y = np.array(dataArray[2:index[0]+2,1], dtype= float)
 
-        ax[n].plot(x,y,label=filename.removesuffix("Freq.txt"))
+        velocity_cumulative = integrate.cumulative_trapezoid(y, x,initial=2)
+        displacement_cumulative = integrate.cumulative_trapezoid(velocity_cumulative,x,initial = 0)
+
+        ax[n].plot(x,displacement_cumulative,label=filename.removesuffix("Freq.txt"))
         # ax[n].set_xticks(np.arange(0,1300,100))    
         # ax[n].set_xlim(0,1300)
         # ax[n].set_ylim(0,0.015)
