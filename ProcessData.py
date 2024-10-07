6a37fe06ac38c4bdcbc7a3a5f4f26b4d1ca355db
hmah689 <hmah689@aucklanduni.ac.nz>
2024-10-07 16:51:14 +1300

    nice force plotty plots

--- ProcessData.py
+++ ProcessData.py
@@ -19,7 +19,9 @@ with open(filename, 'r') as file:
 
 
 plt.figure()
-plt.title('Force vs DC Offset')
+plt.title('Force vs DC')
+plt.xlabel("DC Offeset (A)")
+plt.ylabel("Break Force (N)")
 
 # #get 0AC trend, ignore first element which is 0AC header
 # y = data[1][1:]
@@ -32,16 +34,22 @@ x = []
 for num in data[0]:
     x.append(float(num))
 
-
-
+plt.grid(True)
+# Enable minor gridlines
+plt.minorticks_on()  # Turn on the minor ticks
+plt.grid(which='minor', color='gray', linestyle=':', linewidth=0.5)  # Customize minor gridlines
 
 plt.plot(x,y)
 plt.savefig("Force-data.png")
 
+
+
+#####---------------------------------------------------------------------------
 # do again for AC
 plt.figure()
-plt.title('Force vs DC Offset')
-
+plt.title('Force vs DC Bias')
+plt.xlabel("DC Offeset (A)")
+plt.ylabel("Break Force (N)")
 
 x = []
 for num in data[0]:
@@ -52,7 +60,12 @@ for i in range(2,6):
     for num in data[i][1:]:
         y.append(float(num))
 
-    plt.plot(x,y,label = data[i][0])
+    plt.plot(x,y,label = data[i][0] + "A AC")
     y = []
+plt.grid(True)
+# Enable minor gridlines
+plt.minorticks_on()  # Turn on the minor ticks
+plt.grid(which='minor', color='gray', linestyle=':', linewidth=0.5)  # Customize minor gridlines
+
 plt.legend()
 plt.savefig("Force-data-ac.png")
